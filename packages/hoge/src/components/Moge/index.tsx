90d13c7d44c2ece748e3934012edbac67c29bf10
piro <piro.haniwa@gmail.com>
2021-12-02 10:58:06 +0900

    feat: pass piyo

--- packages/hoge/src/components/Moge/index.tsx
+++ packages/hoge/src/components/Moge/index.tsx
@@ -1,5 +1,5 @@
-import PiyoOne, { PiyoOneProps } from "piyo/PiyoOne";
-import PiyoTwo from "piyo/PiyoTwo";
+import PiyoOne, { PiyoOneProps } from "piyo/dist/PiyoOne";
+import PiyoTwo from "piyo/dist/PiyoTwo";
 import React from "react";
 
 type MogeProps = Pick<PiyoOneProps, "piyo">;
